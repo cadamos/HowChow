b7c47b1948e42abc42e828b7d2d7fab01b7882bb
cadamos <31325509+cadamos@users.noreply.github.com>
2019-07-20 14:24:24 -0400

    comment out unfinished tests

--- src/main/java/driver/Test.java
+++ src/main/java/driver/Test.java
@@ -1,42 +1,73 @@
 package driver;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import model.Dish;
 import model.Review;
+import model.Tag;
+import service.DishService;
 import service.ReviewService;
+import service.TagService;
 
 public class Test {
 	public static void main(String[] args) {
 		
-//		Tag asian = TagService.selectTagByName("Asian");
-//		Tag italian = TagService.selectTagByName("Italian");
-//		Tag mexican = TagService.selectTagByName("Mexican");
-//		Tag chicken = TagService.selectTagByName("Chicken");
-//		Tag pasta = TagService.selectTagByName("Pasta");
+//		Tag beef = TagService.selectTagByName("Beef");
+//		Tag vegan = TagService.selectTagByName("Vegan");
+//		Tag dessert = TagService.selectTagByName("Dessert");
+//		Tag chocolate = TagService.selectTagByName("Chocolate");
+//		Tag pizza = TagService.selectTagByName("Pizza");
+//		Tag burger = TagService.selectTagByName("Burger");
+//		Tag hotdog = TagService.selectTagByName("Hotdog");
+//		Tag fruit = TagService.selectTagByName("Fruit");
 //		
-//		List<Tag> padThaiTags = new ArrayList<Tag>();
-//		padThaiTags.add(asian);
-//		padThaiTags.add(chicken);
+//		TagService.insertTag(beef);
+//		TagService.insertTag(vegan);
+//		TagService.insertTag(dessert);
+//		TagService.insertTag(chocolate);
+//		TagService.insertTag(pizza);
+//		TagService.insertTag(burger);
+//		TagService.insertTag(hotdog);
+//		TagService.insertTag(fruit);
 //		
-//		List<Tag> alfredoTags = new ArrayList<Tag>();
-//		alfredoTags.add(italian);
-//		alfredoTags.add(chicken);
+//		List<Tag> moltenLavaCakeTags = new ArrayList<Tag>();
+//		moltenLavaCakeTags.add(dessert);
+//		moltenLavaCakeTags.add(chocolate);
+//		moltenLavaCakeTags.add(TagService.selectTagByName("Sweet"));
 //		
-//		List<Tag> tacosTags = new ArrayList<Tag>();
-//		tacosTags.add(mexican);
+//		List<Tag> chicagoDogTags = new ArrayList<Tag>();
+//		chicagoDogTags.add(beef);
+//		chicagoDogTags.add(hotdog);
 //		
-//		List<Tag> ramenTags = new ArrayList<Tag>();
-//		ramenTags.add(asian);
-//		ramenTags.add(pasta);
+//		List<Tag> pineapplePizzaTags = new ArrayList<Tag>();
+//		pineapplePizzaTags.add(pizza);
+//		pineapplePizzaTags.add(fruit);
 //		
-//		Dish padThai = new Dish("/assets/images/padThai.jpg", "Pad Thai", "Pad thai is made with soaked dried rice noodles, which are stir-fried with eggs, chopped firm tofu, and flavored with tamarind pulp, fish sauce, dried shrimp, garlic or shallots, red chili pepper and palm sugar.", padThaiTags, "Thai Lao Restaurant");
-//		Dish alfredo = new Dish("/assets/images/alfredo.jpg", "Chicken Alfredo", "This combines simple, fresh ingredients like butter, cream and parmesan cheese to make a rich topping to our fettuccine pasta. Then it is topped with tender, sliced grilled chicken.", alfredoTags, "Assagio's");
-//		Dish tacos = new Dish("/assets/images/tacos.jpg", "Tacos", "A traditional Mexican dish consisting of a corn or wheat tortilla folded or rolled around a filling. A taco can be made with a variety of fillings, including beef, pork, chicken, seafood, vegetables, and cheese.", tacosTags, "El Mariachi Bar and Grill");
-//		Dish ramen = new Dish("/assets/images/ramen.jpg", "Ramen", " a Japanese dish with a translation of 'pulled noodles' (originating in northwest China). It consists of Chinese-style wheat noodles served in a meat or (occasionally) fish-based broth.", ramenTags, "Shokudo");
-//		DishService.insertDish(padThai);
-//		DishService.insertDish(alfredo);
-//		DishService.insertDish(tacos);
-//		DishService.insertDish(ramen);
+//		List<Tag> beefAndBroccoliTags = new ArrayList<Tag>();
+//		beefAndBroccoliTags.add(TagService.selectTagByName("Asian"));
+//		beefAndBroccoliTags.add(beef);
+//		
+//		List<Tag> avocadoToastTags = new ArrayList<Tag>();
+//		avocadoToastTags.add(vegan);
+//		
+//		List<Tag> ramenBurgerTags = new ArrayList<Tag>();
+//		ramenBurgerTags.add(TagService.selectTagByName("Asian"));
+//		ramenBurgerTags.add(TagService.selectTagByName("Pasta"));
+//		ramenBurgerTags.add(burger);
+//		
+//		Dish moltenLavaCake = new Dish("assets/images/moltenLavaCake.jpg", "Molten Lava Cake", "Chocolate cake with a molten chocolate center, topped with vanilla ice cream in a chocolate shell. Big enough to share, too good to actually do it.", moltenLavaCakeTags, "Chili's");
+//		Dish chicagoDog = new Dish("assets/images/chicagoDog.jpg", "Chicago Dog", "Chicago Hot Dog ... Vienna All-Beef served on a steamed poppy seed bun with mustard,relish,onion,tomato,pickle,sport peppers, & celery salt.", chicagoDogTags, "Luke's");
+//		Dish pineapplePizza = new Dish("assets/images/pineapplePizza.jpg", "Pineapple Pizza", "What makes this pizza Hawaiian? A classic combo of tomato sauce, cheese, ham and pineapple. In addition to being super-easy, it’s a great way to use up leftover ham.", pineapplePizzaTags, "Hawaii Pizza Hut");
+//		Dish beefAndBroccoli = new Dish("assets/images/beefAndBroccoli.jpg", "Beef and Broccoli", "A classic favorite. Tender beef and fresh broccoli in a ginger soy sauce.", beefAndBroccoliTags, "Panda Express");
+//		Dish avocadoToast = new Dish("assets/images/avocadoToast.jpg", "Avocado Toast", "Avocado toast is a type of open sandwich or toast made with mashed avocado and salt, pepper, and citrus juice on toast.", avocadoToastTags, "Sargasso");
+//		Dish ramenBurger = new Dish("assets/images/ramenBurger.jpg", "Ramen Burger", "Instant ramen becomes a nearly-instant burger bun in this fun, and tasty, spin on a traditional hamburger seasoned with soy sauce and sesame oil.", ramenBurgerTags, "L&L Hawaiian Barbeque");
+//		DishService.insertDish(moltenLavaCake);
+//		DishService.insertDish(chicagoDog);
+//		DishService.insertDish(pineapplePizza);
+//		DishService.insertDish(beefAndBroccoli);
+//		DishService.insertDish(avocadoToast);
+//		DishService.insertDish(ramenBurger);
 
 	}
 }
