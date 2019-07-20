ebb708a172d95bd79163eba49c2a6ca46bb971f0
karim728 <adambo1@students.towson.edu>
2019-07-20 12:09:57 -0400

    selenium/cucumber test

--- src/test/java/drivertest/SearchTest.java
+++ src/test/java/drivertest/SearchTest.java
@@ -16,57 +16,58 @@ import org.openqa.selenium.support.ui.Select;
 
 import pages.SearchMain;
 
-
-
 public class SearchTest {
-	
+
 	public static ChromeDriver driver;
 	public static SearchMain wikimain;
-	
+
 	@BeforeSuite
 	public void setUp() {
-		
+
 		File file = new File("src/main/resources/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-		
+
 		driver = new ChromeDriver();
 		wikimain = new SearchMain(driver);
 	}
-	
+
 	@BeforeMethod
 	public void before() {
-		
+
 		driver.get("http://localhost:4200/dish-list/");
-		
+
 	}
-	
+
 	@Test(priority = 1)
 	public void search() {
-		WebElement sbox= wikimain.getSearchbar();
-		//wikimain.getSearchbar().click();
-		
+		WebElement sbox = wikimain.getSearchbar();
+		// wikimain.getSearchbar().click();
+
 		try {
 			Thread.sleep(8000);
-			//sbox.sendKeys("Pasta");
+			// sbox.sendKeys("Pasta");
 			sbox.click();
-			
-			
+
 			Thread.sleep(4000);
-			
-			//System.out.println(dropdown + "llllllllllllllllllllllllllllllllllllllllllllll");
-			WebElement dropdown= driver.findElement(By.xpath("/html/body/app-root/app-navbar/nav/form/div/sui-multi-select/div[2]/sui-select-option[1]"));
+
+			WebElement dropdown = driver.findElement(By
+					.xpath("/html/body/app-root/app-navbar/nav/form/div/sui-multi-select/div[2]/sui-select-option[1]"));
 			Thread.sleep(4000);
 			dropdown.click();
-			//sbox.sendKeys(Keys.ENTER);
-			//sbox.click();
+			// sbox.sendKeys(Keys.ENTER);
+			// sbox.click();
+			Thread.sleep(4000);
+			WebElement review = driver
+					.findElement(By.xpath("/html/body/app-root/app-dish-list/div/div/app-dish-list-item[3]/div/img"));
+			Thread.sleep(4000);
+			review.click();
+			Thread.sleep(4000);
 			Assert.assertEquals(driver.getTitle(), "HowChowApp");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 	}
-	
-	
 
 }
